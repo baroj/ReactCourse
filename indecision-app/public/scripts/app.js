a22bddf0fc98ce182073265fd5f27f2c09363bc0
baroj <johnattanbarona@gmail.com>
2018-08-09 17:00:16 -0400

    Learned jsx javascript expressions

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -2,11 +2,90 @@
 
 console.log('app.js is running');
 
+// create app object title/subtitle
+//user title subtitle in template
+// render template
+
+var app = {
+    title: 'Indecision App',
+    subtitle: 'This is the subtitle for the app'
+};
+
 // JSX - Javscript XML 
 var template = React.createElement(
-  'h1',
-  null,
-  ' Indecision App '
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        ' ',
+        app.title,
+        ' '
+    ),
+    React.createElement(
+        'p',
+        null,
+        ' ',
+        app.subtitle,
+        ' '
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            ' Item One '
+        ),
+        React.createElement(
+            'li',
+            null,
+            ' Item Two '
+        )
+    )
 );
+
 var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
+
+//Create a templateTwo var JSX Expression
+// div 
+//  h1 -> Johnattan Barona 
+//  p -> Age: 25
+//  p-> Location: Florida
+//  Render Template Two instead of template
+
+
+var user = {
+    name: 'Johnattan',
+    age: 25,
+    location: 'Florida'
+};
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        ' ',
+        user.name,
+        ' '
+    ),
+    React.createElement(
+        'p',
+        null,
+        ' Age: ',
+        user.age,
+        ' '
+    ),
+    React.createElement(
+        'p',
+        null,
+        ' Location: ',
+        user.location,
+        ' '
+    )
+);
+
+//ReactDOM.render(templateTwo, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,6 +1,50 @@
 console.log('app.js is running');
 
+// create app object title/subtitle
+//user title subtitle in template
+// render template
+
+var app = {
+  title: 'Indecision App',
+  subtitle: 'This is the subtitle for the app'  
+};
+
+
 // JSX - Javscript XML 
-var template = <h1> Indecision App </h1>;
+var template = (
+    <div>
+        <h1> {app.title} </h1>
+        <p> {app.subtitle} </p>
+        <ol>
+            <li> Item One </li>
+            <li> Item Two </li>
+        </ol>
+    </div>
+);
+
 var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot );
+
+//Create a templateTwo var JSX Expression
+// div 
+//  h1 -> Johnattan Barona 
+//  p -> Age: 25
+//  p-> Location: Florida
+//  Render Template Two instead of template
+
+
+var user = {
+    name : 'Johnattan',
+    age : 25,
+    location : 'Florida'
+};
+
+var templateTwo = (
+    <div>
+        <h1> {user.name} </h1>
+        <p> Age: {user.age} </p>
+        <p> Location: {user.location} </p>
+    </div>
+);
+
+//ReactDOM.render(templateTwo, appRoot);
