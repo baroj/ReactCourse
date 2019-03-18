626c8b9a21b7e4f4b85a733f840697138dca3586
Johnattan Barona <johnattanbarona@gmail.com>
2019-03-18 09:28:50 -0400

    Bringin redux into our react project

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import {addExpense} from './actions/expenses';
+import {setTextFilter} from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+const store = configureStore();
+
+const expenseOne = store.dispatch(addExpense({description:'water bill', amount:100, createdAt:1000}));
+const expenseTwo = store.dispatch(addExpense({description:'gas bill', amount:300,  createdAt:-1000}));
+store.dispatch(setTextFilter('water'));
+
+const state = store.getState();
+const visb = getVisibleExpenses(state.expenses, state.filters);
+console.log(visb);
 
 ReactDOM.render(<AppRouter/>, document.getElementById('app'));
