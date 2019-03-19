4ec8b945ef1b28658fce75f5f010f45aaf63e912
Johnattan Barona <johnattanbarona@gmail.com>
2019-03-19 09:17:52 -0400

    React Redux with Expensify

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import {addExpense} from './actions/expenses';
@@ -10,12 +11,22 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-const expenseOne = store.dispatch(addExpense({description:'water bill', amount:100, createdAt:1000}));
-const expenseTwo = store.dispatch(addExpense({description:'gas bill', amount:300,  createdAt:-1000}));
+const expenseOne = store.dispatch(addExpense({description:'water bill', amount:100, createdAt:0}));
+const expenseTwo = store.dispatch(addExpense({description:'gas bill', amount:300,  createdAt:250}));
 store.dispatch(setTextFilter('water'));
+
+setTimeout(()=>{
+    store.dispatch(setTextFilter('bill'));
+} , 3000);
 
 const state = store.getState();
 const visb = getVisibleExpenses(state.expenses, state.filters);
 console.log(visb);
 
-ReactDOM.render(<AppRouter/>, document.getElementById('app'));
+const jsx = (
+    <Provider store = {store}>
+        <AppRouter/>
+    </Provider>
+);
+
+ReactDOM.render( jsx, document.getElementById('app'));
