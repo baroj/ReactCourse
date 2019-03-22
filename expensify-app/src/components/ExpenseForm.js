8eefb44b3167a70e22d2b2051234677072e1e10e
Johnattan Barona <johnattanbarona@gmail.com>
2019-03-22 09:14:05 -0400

    Edit Expense, Redux Tools, Date Range Filtering

--- expensify-app/src/components/ExpenseForm.js
+++ expensify-app/src/components/ExpenseForm.js
@@ -5,14 +5,18 @@ import 'react-dates/lib/css/_datepicker.css';
 
 
 export default class ExpenseForm extends React.Component {
-    state = {
-        description : '',
-        amount : '',
-        note: '',
-        createdAt: moment(),
-        calendarFocused: false,
-        error: ''
-    };
+    constructor(props){
+        super(props);
+
+        this.state= {
+            description : props.expense ? props.expense.description : '',
+            amount : props.expense ? (props.expense.amount /100).toString() : '',
+            note: props.expense? props.expense.note : '',
+            createdAt: props.expense? moment(props.expense.createdAt) : moment(),
+            calendarFocused: false,
+            error: ''
+        }
+    }
 
     onDescriptionChange = (e) =>{
         const description = e.target.value;
@@ -92,7 +96,7 @@ export default class ExpenseForm extends React.Component {
                         onChange={this.onNoteChange}>
                     </textarea>
                     <button>
-                        Add Expense
+                       Add Expense
                     </button>
                 </form>
             </div>
